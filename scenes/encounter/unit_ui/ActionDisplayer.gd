d4fd906120a1232177f3222e5c6c98ff39072964
Ben Newey <35576749+bbnewey15@users.noreply.github.com>
2024-01-10 09:57:37 -0600

    working attack label

--- scenes/encounter/unit_ui/ActionDisplayer.gd
+++ scenes/encounter/unit_ui/ActionDisplayer.gd
@@ -2,11 +2,13 @@ extends Control
 class_name ActionDisplayer
 
 @onready var action_label: Label = %ActionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	action_label.hide()
-	
+	#action_label.hide()
+	action_label.set_self_modulate(Color(0,0,0,0)) 
 	pass # Replace with function body.
 
 
@@ -15,22 +17,9 @@ func _process(delta: float) -> void:
 	pass
 
 func display_action( action_data : ActionData, slot_data: SlotData) :
-	set_fade_progress(0)
-	action_label.show()
+	
 	action_label.text = action_data.name
-	return await fade_in_label()
-
-func set_fade_progress( progress: float):
-	assert(progress >= 0 and progress <= 1)
-	var shader_material : Material = action_label.get_material()
-	shader_material.set_shader_parameter("fade_progress", progress)
-	
-
-func fade_in_label():
-	var shader_material : Material = action_label.get_material()
-	var fade_animation = create_tween()
-	fade_animation.set_ease(Tween.EASE_IN_OUT)
-	fade_animation.set_trans(Tween.TRANS_LINEAR)
-	fade_animation.tween_property(shader_material, "shader_parameter/fade_progress", 1.0, 1.5).from(0)
-	return await fade_animation.finished
-	
+	animation_player.play("fade_in_out")
+	await animation_player.animation_finished
+	action_label.set_self_modulate(Color(0,0,0,0)) 
+	return

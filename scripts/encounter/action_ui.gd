d29661820b7abee6dfadfe07b7503d8cdd74343a
Ben Newey <35576749+bbnewey15@users.noreply.github.com>
2024-01-12 12:15:06 -0600

    Most done level up

--- scripts/encounter/action_ui.gd
+++ scripts/encounter/action_ui.gd
@@ -6,7 +6,6 @@ var currentUnit: UnitDataTest
 var encounter_manager : EncounterManager
 
 
-@onready var move_actions = %MoveActions
 @onready var unit_actions = %UnitActions
 @onready var actions = %Actions
 @onready var action_scene = preload("res://scenes/encounter/actions/action.tscn")
@@ -32,47 +31,6 @@ func add_action(action_data: ActionData):
 	action_node.set_action_data(action_data)
 	
 
-func move_unit(direction: GameData.MOVE_DIRECTION ):
-	assert(GameData.ui_active_slot_data)
-	assert(encounter_manager)
-	
-	print("direction" )
-	print(direction)
-	var slot = GameData.ui_active_slot_data.current_slot
-	var moving_slot_column : UnitColumn = slot.get_node("../../../")
-	var move_to_column_index
-	match direction:
-		GameData.LEFT:
-			print("LEFT")
-			
-			move_to_column_index = moving_slot_column.column_data.colIndex - 1
-			
-		GameData.RIGHT:
-			print("RIGHT")
-			move_to_column_index = moving_slot_column.column_data.colIndex + 1
-			
-	assert(move_to_column_index != null)
-	
-	if move_to_column_index >= 0 && move_to_column_index <= 5:
-		moving_slot_column.unit_grid.remove_child(slot)
-		
-		#Add to new column
-		var column_moved_to: UnitColumn = encounter_manager.columnGroup.column_dict[GameData.getColumnStringByIndex(move_to_column_index)]
-		column_moved_to.add_slot( GameData.ui_active_slot_data, false)
-		update_actionUI()
-		#Remove from old column
-		slot.queue_free()
-	else:
-		return
-
-
-func _on_move_gui_input(event, left_or_right):
-	if event is InputEventMouseButton \
-		and (event.button_index == MOUSE_BUTTON_LEFT) \
-		and event.is_pressed():
-			print("Move Left Clicked %s : %s" % [event, left_or_right] )
-			move_unit(left_or_right)
-	
 	
 func on_unit_selected(slot_data: SlotData, button: int) -> void:
 	# Actions are only performed from ally units
@@ -141,7 +99,6 @@ func update_actionUI() -> void:
 					self.add_action(action)
 					
 				
-				move_actions.hide()
 				unit_actions.show()
 			else:
 				self.update_active_action_button(null)
@@ -149,12 +106,10 @@ func update_actionUI() -> void:
 		"PostFight":
 			if GameData.ui_active_slot_data and !GameData.ui_active_slot_data.isEnemyUnit:
 				self.show()
-				move_actions.show()
 				unit_actions.hide()
 			else:
 				self.hide()
 		_:
-			move_actions.hide()
 			unit_actions.hide()
 			self.hide()
 			
